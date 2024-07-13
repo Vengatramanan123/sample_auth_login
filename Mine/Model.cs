1f4cd1f474278508f92fa7e2f85b9c2731f85988
Vengatramanan <104194902+Vengatramanan123@users.noreply.github.com>
2024-07-13 19:48:06 +0530

    Add files via upload

---
+++ Mine/Model.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mine
+{
+    class Model
+    {
+        public string user;
+        public int usercode;
+        public string user_email;
+        private string password;
+
+        public Model(string user_name, int user_code, string useremail)
+        {
+            user = user_name;
+            usercode = user_code;
+            user_email = useremail;
+           
+            
+        }
+
+        public string Password
+        {
+            get { return Password; }
+            set
+            {
+                password = value;
+
+            }
+        }
+
+        public bool validation()
+        {
+            if(user == "Admin" && usercode == 2341)
+            {
+                Console.WriteLine("User Logged In Successfully");
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Mine/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Mine;
+
+namespace Mine
+{
+    class Mine
+    {   
+        static void Main()
+        {
+            string user_name;
+            string user_email;
+            int user_code;
+            string user_password;
+            
+            try
+            {
+                
+                Console.WriteLine("Welcome to Simple Auth Login\n");
+
+                Console.Write("Enter your username : ");
+                user_name = Console.ReadLine();
+
+                Console.Write("Enter your code : ");
+                user_code = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter your email : ");
+                user_email = Console.ReadLine();
+
+                Console.Write("Enter your password : ");
+                user_password = Console.ReadLine();
+
+                Model login = new Model(user_name, user_code, user_email);
+                login.Password = user_password;
+
+                if (user_password !="")
+                {
+                    login.validation();
+                }
+                else
+                {
+                    Console.WriteLine("\nAuthentication Failed");
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+        }
+    }
+}
